b83ef3433a8ced703cb9dd40574cf2de320b9c90
yslaa <crislhan.fabila@tup.edu.ph>
2024-03-27 18:02:50 +0800

    Refactor product model schema

--- models/product.js
+++ models/product.js
@@ -1,65 +1,46 @@
 const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
+const {
+  RESOURCE
+} = require("../constants/index");
 
 const productSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: [true, "Please enter a user"],
-    ref: RESOURCE.USER,
-  },
   product_name: {
     type: String,
     required: [true, "Please enter a product name"],
     maxLength: [30, "The product name cannot exceed 30 characters"],
   },
+  brand: [{
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "Please enter a brand"],
+    ref: RESOURCE.BRAND,
+  }, ],
   type: {
     type: String,
-    required: [true, "Please enter a type of your product"],
-  },
-  class: {
-    type: String,
-    required: [true, "Please enter a class of your product"],
-  },
-  variant: {
-    type: String,
-    enum: ["Local", "International"],
-    default: "Local",
+    enum: ["Door Accessories", "Machinery Equipment", "Hand Tools", "Safety and Security", "Power Tools", "Painting", "Electrical", "Lighting", "Building Materials"],
   },
   price: {
     type: Number,
     required: [true, "Please enter a price"],
   },
-  wishlist: [
-    {
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: RESOURCE.USER,
-      },
-    },
-  ],
-  stock: {
+  stock: [{
     type: Number,
-    required: [true, "Enter the quantity of the Product"],
-    maxlength: [4, "Product cant exceed up to 4 digits"],
-    default: 0,
-  },
-  image: [
-    {
-      public_id: {
-        type: String,
-        required: true,
-      },
-      url: {
-        type: String,
-        required: true,
-      },
-      originalname: {
-        type: String,
-        required: true,
-      },
+    required: [true, "Stocks field required"],
+    min: [1, "Stocks field must be at least 1"],
+  }, ],
+  image: [{
+    public_id: {
+      type: String,
+      required: true,
     },
-  ],
+    url: {
+      type: String,
+      required: true,
+    },
+    originalname: {
+      type: String,
+      required: true,
+    },
+  }, ],
 });
 
 module.exports = mongoose.model(RESOURCE.PRODUCT, productSchema);
